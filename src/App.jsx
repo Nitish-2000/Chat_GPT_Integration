12f11f1b0154a08d1a3038e8bfb0be57ae3d13fd
“Nitish-2000” <nitishmani111@gmail.com>
2024-01-14 17:03:26 +0530

    code updated

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import './App.css';
+
+
+function App() {
+return <>
+ <div className='Main'>
+<section id="sidebar1">
+
+<div className='leftbox'>
+<h1 className='sidebar1-heading1'>PROPTIMIZE</h1>
+<button className='New-Chart'>New Chart</button>
+<p className='p-pinned-charts'>Pinned Charts</p>
+<div className='Pinned-charts-buttons'>
+<button className='Pinned-charts-button1'>What are the best places to live in Ottawa</button><br/>
+<button className='Pinned-charts-button2'>What are the best places to live in Ottawa</button><br/>
+<button className='Pinned-charts-button3'>What are the best places to live in Ottawa</button><br/>
+</div>
+<p className='P-Today'>Today</p>
+<button className='P-Today-button'>What are the best places to live in Ottawa</button>
+<p className='P-Pervious'>Previous 7 Days</p>
+<div className='Pervious-buttons'>
+<button className='Pervious-button1'>What are the best places to live in Ottawa</button><br/>
+<button className='Pervious-button2'>What are the best places to live in Ottawa</button><br/>
+<button className='Pervious-button3'>What are the best places to live in Ottawa</button><br/>
+</div>
+<p className='P-username'>Nitish Mani</p>
+</div>
+</section>
+<section id="sidebar2">
+<div className='rightbox'>
+<h1 className='sidebar2-heading1' >Welcome Can!,What are you working on today?</h1>
+<h2 className='sidebar2-heading2'>Sample Design-1</h2>
+<p className='P-Content'>"Use Search feature to analyse neighbourhood demographics"</p>
+<button className='Auto-Search1'>What are the best places to live in Ottawa as a tech worker?<br/>
+Search web for nearby amenities.</button>
+<button className='Auto-Search2'>What are the best places to live in Ottawa as a tech worker?<br/>
+Search web for nearby amenities.</button><br/>
+<input type='text' className='Message-bar' placeholder='Message Echo..'></input >
+</div>
+</section>
+</div>
+
+</>
+}
+
+
+export default App;
